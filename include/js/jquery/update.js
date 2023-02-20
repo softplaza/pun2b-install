ddcfacb9ff11b752e47bb473daf6e36cf12fd0f3
SwiftManager <92275063+swiftprojectmanager@users.noreply.github.com>
2023-02-19 23:01:41 -0800

    rebuild 2/19/2023

--- include/js/jquery/update.js
+++
@@ -1,8 +0,0 @@
-
-var jQ = new Object({
-	update_version:function() {
-		$.get( PUNBB.env.base_url+'/include/js/functions.php',function(){
-            window.location.reload();
-	    });
-    }
-});
